07ed5aa1c06f8e8b68e8db06c42f3f0ce26b4771
christianiandv <christian.i.de.vera@accenture.com>
2026-03-13 10:53:25 +0000

    Update manage.py or confirm all backend code is up to date.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-5y_h4e^g#*!6szeher6yki6(83xk*oo#n))(nm7g+xs5l+j-ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
